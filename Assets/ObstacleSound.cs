801184bc02ea15a6cdaf47cdc14993d184e90ac5
marcoscirea <marco.prolog@gmail.com>
2014-09-20 19:33:00 +0200

    RUN ANITA! RUUUUUUUUN!

---
+++ Assets/ObstacleSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleSound : MonoBehaviour {
+
+	public AudioClip[] audios;
+	int selected;
+	AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		selected = Random.Range(0, audios.Length);
+		source = GameObject.FindGameObjectWithTag("Player").audio;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		source.PlayOneShot(audios[selected]);
+	}
+}
